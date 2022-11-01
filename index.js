3de668ca2cd9cd41e5490624af6fe2e4d64246bf
mohammad-sumon <sumon3011@gmail.com>
2022-11-02 00:15:21 +0600

    67.3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -22,11 +22,20 @@ async function run() {
   try {
     const serviceCollection = client.db("geniusCar").collection("services");
 
+    // CRUD - READ(ALL DATA)
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    // CRUD - READ(SPECIFIC DATA)
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
